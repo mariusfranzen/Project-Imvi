7389b0a583d74923609159123b6b0763a5d240a0
Marius Franzén <marius.franzen1@gmail.com>
2019-11-21 13:33:07 +0100

    Moved menu to separate UserControl

---
+++ Models/ImageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Imvi.Models
+{
+    class ImageModel
+    {
+    }
+
+    public class MainImage : INotifyPropertyChanged
+    {
+
+    }
+}
